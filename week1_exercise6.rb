86c57da84a38ea18c545fd5eba91fbcbc7ecdf4b
Scott Kopelchuk <scott.kopelchuk@me.com>
2012-11-17 16:41:32 -0600

    adding week 1 update

--- week1_exercise6.rb
+++ week1_exercise6.rb
@@ -1,4 +1,15 @@
-
+=begin
+doctest: convert(-40)
+>> convert 40
+=> -40
+doctest: convert(98.6)
+>> convert 98.6
+>> 37.0
+docttest: convert(98)
+The following test is to test a delta, it is "close enough"
+>> (convert (98) * 10000 ).round / 10000.0
+=> 36.666667
+=end
 def convert(fahrenheit)
   celsius = (fahrenheit-32) / 1.8
 end
