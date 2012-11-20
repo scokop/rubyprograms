ea63ceb45b2fa57d0b6d4139da3748ca24ba9dd2
Victor Goff <keeperotphones@gmail.com>
2012-11-19 20:26:00 -0600

    added tests for ensuring a string return, rather than an Array

--- 2week/wk2e1.rb
+++ 2week/wk2e1.rb
@@ -1,18 +1,17 @@
 =begin
 doctest: This tests the output of the string conversion
->> converter(s)
-=> ["Line 1: Welcome to the forum.","Line 2: Here you can learn Ruby.","Line 3: Along with other members."]
+>> multi_line_string = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+>> converter(multi_line_string)
+=> "Line 1: Welcome to the forum.\nLine 2: Here you can learn Ruby.\nLine 3: Along with other members.\n"
 
 =end
 
-
-def converter(s)
-  array = []
-  s.split("\n").each_with_index {|line,index| array << "Line #{index+1}: #{line}"}
-  return array
+def linify(multi_line_string)
+  multi_line_string.split(/\n/).map.with_index(1) {|line, index|
+    "Line #{index}: #{line}\n" }.join
 end
 
-s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
-puts converter(s) 
-
-
+if __FILE__ == $0
+  s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+  puts linify(s) 
+end
