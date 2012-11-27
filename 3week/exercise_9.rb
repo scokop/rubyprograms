88f3950739a2fae9fe7d34e2287ad4c9280aab5d
Scott Kopelchuk <scott.kopelchuk@me.com>
2012-11-27 16:29:02 -0600

    Changed exercise 9 to add punctuation and capitalization

--- 3week/exercise_9.rb
+++ 3week/exercise_9.rb
@@ -1,15 +1,29 @@
-=begin
-doctest: This tests to see if the sentence gets reversed.
->> reverse_sentence("This is a test")
-=> "test a is This"
+=begin                                                              
+doctest: This tests to see if the sentence gets reversed.           
+>> reverse_sentence("this is a test")                               
+=> "test a is this"                                                 
+doctest: This puts the period at the end                            
+>> reverse_sentence("this is a test.")                              
+=> "test a is this."                                                
+doctest: it also capitalizes properly                               
+>> reverse_sentence("This puts the proper sentence capitalization.")
+=> "Capitalization sentence proper the puts this."                  
 =end
 
 def reverse_sentence(text)
+  punctuation = text.slice!(/[.?!]/)
   words = text.split
-  words.reverse!.join(" ")
+  if words[0] != words[0].downcase
+    words[0].downcase!
+    words.last.capitalize!
+  end
+  if punctuation.nil? then punctuation = "" end
+  words.reverse!.join(" ") << punctuation
+end 
+
+if __FILE__ == $0
+  puts "Please enter a string."
+  text = gets.chomp
+
+  puts reverse_sentence(text)
 end
-
-puts "Please enter a string."
-text = gets.chomp
-
-puts reverse_sentence(text)
