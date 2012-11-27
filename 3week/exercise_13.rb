eca6da82685755e7841c9c7e908404228f1d509b
Scott Kopelchuk <scott.kopelchuk@me.com>
2012-11-27 17:38:44 -0600

    Changed exercise 13 to Ruby 1.9 hash syntax

--- 3week/exercise_13.rb
+++ 3week/exercise_13.rb
@@ -1,1 +1,7 @@
-record = { :name => "Satish", :email => "mail@satishtalim.com", :phone => "919371006659"}
+record = {name: "Satish", email: "mail@satishtalim.com", phone: "919371006659"}
+
+puts <<-EOS % record
+record[:name]  = '%{name}'
+record[:email] = '%{email}'
+record[:phone] = '%{phone}'
+EOS
