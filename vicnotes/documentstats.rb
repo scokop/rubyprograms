445d5c692965d0f82c06c6a2fc25f4516603d033
Victor Goff <keeperotphones@gmail.com>
2012-11-27 03:45:26 -0600

    I prefer the %s %s formatting, it shows the number as stored

--- vicnotes/documentstats.rb
+++ vicnotes/documentstats.rb
@@ -20,7 +20,7 @@ end
 
 def output_results(text_results)
   text_results.each_key do | key |
-    puts "There are %d %s." % text_results[key]
+    puts "There are %s %s." % text_results[key]
   end
 end
 
