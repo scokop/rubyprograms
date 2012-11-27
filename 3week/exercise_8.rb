c8684ef9f43661f63fa7fcb2d682a49f695aaf43
Scott Kopelchuk <scott.kopelchuk@me.com>
2012-11-27 15:19:23 -0600

    Made changes to exercise 8 to simplify conditionals

--- 3week/exercise_8.rb
+++ 3week/exercise_8.rb
@@ -1,12 +1,15 @@
 
-1.upto(100) do |number|
-  if number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
-  elsif number % 3 == 0
-    puts "Fizz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  else
-  puts  number
+def fizzbuzz
+  1.upto(100) do |number|
+    fizz_buzz_output = ""
+    if number % 3 == 0 then fizz_buzz_output << "Fizz" end
+    if number % 5 == 0 then fizz_buzz_output << "Buzz" end
+    if fizz_buzz_output.empty?
+        puts number 
+    else
+        puts fizz_buzz_output
+    end
   end
 end
+
+fizzbuzz
