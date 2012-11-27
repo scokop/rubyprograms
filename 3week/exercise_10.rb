2f9327e3aea9116191d2a62d8d565c861ca032cd
Scott Kopelchuk <scott.kopelchuk@me.com>
2012-11-27 16:39:50 -0600

    Simplified exercise 10 by using inject

--- 3week/exercise_10.rb
+++ 3week/exercise_10.rb
@@ -1,17 +1,17 @@
-=begin
-
-doctest: This tests to see if an array sums correctly. 
->> sum_array([1,2,3,4,5])
-=> 15
-
-=end
+=begin                                                          
+doctest: This tests to see if an array sums correctly..         
+>> sum_array([1,2,3,4,5])                                       
+=> 15                                                           
+doctest: This tests to see if an array of floats sums correctly 
+>> sum_array([1.5, 2.5, 3.5, 4.5, 5.5])                         
+=> 17.5                                                         
+doctest: This tests to see if an array of strings sums correctly
+>> sum_array(%w[one two three four])                            
+=> "onetwothreefour"                                            
+=end  
 
 def sum_array(array)
-  total = 0
-  array.each do |number|
-    total += number.to_i
-  end
-  return total
+  array.inject(:+)
 end
 
 if __FILE__ == $0
