a9d38b9fc686b3da329a97c73b5398bf48e099c3
Victor Goff <keeperotphones@gmail.com>
2012-11-27 01:12:57 -0600

    made the functional unique values in analyzer, iterated through to get report

--- vicnotes/documentstats.rb
+++ vicnotes/documentstats.rb
@@ -5,13 +5,13 @@ end
 def text_analyzer(text)
   text_results = {}  
   text_results[:number_of_lines] = text.length, 'lines'
-  text_results[:text_as_string] = text.join
-  text_results[:character_count] = text_results[:text_as_string].length, 'characters'
-  text_results[:no_whitespace] = text_results[:text_as_string].delete(" ")
-  text_results[:character_count_no_spaces] = text_results[:no_whitespace].length, 'characters without spaces'
-  text_results[:number_of_words] = text_results[:text_as_string].split(" ").length, 'words'
-  text_results[:number_of_sentences] = text_results[:text_as_string].split(/\.|\?|!/).length, 'sentences'
-  text_results[:number_of_paragraphs] = text_results[:text_as_string].split(/\n\n/).length, 'paragraphs'
+  text_as_string = text.join
+  text_results[:character_count] = text_as_string.length, 'characters'
+  no_whitespace = text_as_string.delete(" ")
+  text_results[:character_count_no_spaces] = no_whitespace.length, 'characters without spaces'
+  text_results[:number_of_words] = text_as_string.split(" ").length, 'words'
+  text_results[:number_of_sentences] = text_as_string.split(/\.|\?|!/).length, 'sentences'
+  text_results[:number_of_paragraphs] = text_as_string.split(/\n\n/).length, 'paragraphs'
   text_results[:characters_per_word] = (text_results[:character_count_no_spaces][0]/text_results[:number_of_words][0].to_f).round(2), 'characters per word'
   text_results[:words_per_sentence] = (text_results[:number_of_words][0]/text_results[:number_of_sentences][0].to_f).round(2), 'words per sentence'
   text_results[:sentences_per_paragraph] = (text_results[:number_of_sentences][0]/text_results[:number_of_paragraphs][0].to_f).round(2), 'sentences per paragraph'
@@ -19,15 +19,10 @@ def text_analyzer(text)
 end
 
 def output_results(text_results)
-  puts "There are #{text_results[:number_of_lines][0]} #{text_results[:number_of_lines][1]}."
-  puts "There are #{text_results[:character_count][0]} #{text_results[:character_count][1]}."
-  puts "There are #{text_results[:character_count_no_spaces][0]} #{text_results[:character_count_no_spaces][1]}."
-  puts "There are #{text_results[:number_of_words][0]} #{text_results[:number_of_words][1]}."
-  puts "There are #{text_results[:number_of_sentences][0]} #{text_results[:number_of_sentences][1]}."
-  puts "There are #{text_results[:number_of_paragraphs][0]} #{text_results[:number_of_paragraphs][1]}."
-  puts "There are #{text_results[:characters_per_word][0]} #{text_results[:characters_per_word][1]}."
-  puts "There are #{text_results[:words_per_sentence][0]} #{text_results[:words_per_sentence][1]}."
-  puts "There are #{text_results[:sentences_per_paragraph][0]} #{text_results[:sentences_per_paragraph][1]}."
+  text_results.each_key do | key |
+    puts "There are #{text_results[key][0]} #{text_results[key][1]}."
+  end
 end
+
 text_results = text_analyzer(retrieve_file)
 output_results(text_results)
